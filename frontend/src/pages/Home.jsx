cab9abd853adf5cf6685ccb4c7569b358235e3ed
alphaa <alifadha1@gmail.com>
2024-04-02 02:54:31 +0700

    add frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:5555/books")
+      .get("https://mern-book-test-backend.vercel.app/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
