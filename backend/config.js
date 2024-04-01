aa2ee42b81b2d432d9552bad2351ca017496b2f5
alphaa <alifadha1@gmail.com>
2024-04-02 02:23:56 +0700

    tes

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = 3000;
 
 export const mongoDBURL =
   "mongodb+srv://root:root@bookstore.xkxpe44.mongodb.net/?retryWrites=true&w=majority&appName=bookstore";
